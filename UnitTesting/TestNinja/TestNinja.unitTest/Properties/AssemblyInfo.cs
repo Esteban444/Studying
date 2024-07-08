e9042800d6e32d5fcdb17ebc6b269db89021af7f
Esteban444 <juanes.x777@gmail.com>
2024-07-08 11:37:09 -0500

    I add project of unit testing.

---
+++ UnitTesting/TestNinja/TestNinja.unitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.unitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.unitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("11dda20f-90e9-41e9-b190-0d10f5380db1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTesting/TestNinja/TestNinja.unitTest/ReservationTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.unitTest
+{
+    //[TestClass] // pruebas con visual MStest
+    [TestFixture]
+    public class ReservationTests
+    {
+        //[TestMethod] pruebas con Mstest
+        /* Al cambiar de MSTest a NUnit se genera un error en Assert y es porque el compilador no sabe de donde usar la
+         * clase Assert se remueve using Microsoft.VisualStudio.TestTools.UnitTesting;, para solucionar. */
+
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+
+            //Act
+            var result  = reservation.CanBeCancelledBy( new User { IsAdmin = true });
+
+            //Assert
+            Assert.That( result, Is.True );
+            //Assert.IsTrue( result ); con Mstest
+
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnTrue()
+        {
+            //Arrange
+            var user = new User ();
+            var reservation = new Reservation { MadeBy = user };
+
+            //Act
+            var result = reservation.CanBeCancelledBy( user );
+
+            //Assert
+            Assert.That(result, Is.True);
+
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancellingReservation_ReturnFalse ()
+        {
+            //Arrange
+            var reservation = new Reservation { MadeBy = new User() };
+
+            //Act
+            var result = reservation.CanBeCancelledBy( new User() );
+
+            //Assert
+            Assert.That(result, Is.False);
+
+        }
+    }
+}
