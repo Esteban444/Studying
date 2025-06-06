e8f4d711948ee5e97a03d17d5fd064c2dcf28ba8
Esteban444 <juanes.x777@gmail.com>
2025-06-06 14:33:01 -0500

    Add project infrastructure.

---
+++ C#/CleanArchitecture/CleanArchitecture.Infrastructure/DependencyInjections.cs
@@ -0,0 +1,13 @@
+﻿namespace CleanArchitecture.Infrastructure;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjections
+{
+    public static IServiceCollection AddInfrastructure( IConfiguration configuration , IServiceCollection services )
+    {
+
+        return services;
+    }
+}
